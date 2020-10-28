6cc7498d37e1d40481bc0ce01d160cc4d7e35c4c
Eghosa Osayande <69315505+yande-eghosa@users.noreply.github.com>
2020-10-28 02:45:02 +0100

    Added queue download function

--- YTV/downloadmanager/dm.py
+++ YTV/downloadmanager/dm.py
@@ -1,15 +1,15 @@
 import os, dill
 #import kivy
 from jnius import autoclass
-
+Request=autoclass("android.app.DownloadManager$Request")
+Query= autoclass("android.app.DownloadManager$Query")
+DownloadManager=dm=autoclass("android.app.DownloadManager")
 activity = autoclass('org.kivy.android.PythonActivity').mActivity
 Context = autoclass('android.content.Context')
 Uri = autoclass('android.net.Uri')
 Environment=ee=autoclass('android.os.Environment')
-
 VERSION=autoclass('android.os.Build$VERSION')
 VERSION_CODES=autoclass('android.os.Build$VERSION_CODES')
-#PackageManager=autoclass('android.content.pm.PackageManager')
 
 def get_build_no():
 	try:
@@ -27,8 +27,15 @@ def get_filename(url):
 	from pathlib import PurePosixPath
 	return PurePosixPath(unquote(urlparse(url).path)).parts[-1]
 
-def has_storage_permission():
-	return False
-
-if __name__ == '__main__':
-    print(has_storage_permission())
+def queue_download(video):
+	request = Request(Uri.parse(video.url))
+	request.setDescription("YTVSeries")
+	request.setTitle(f"{get_filename(video.url)}")
+	request.setNotificationVisibility(Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+	request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, os.path.join('YTVSeries',f"{get_filename(video.url)}"))	
+	manager =  activity.getSystemService(Context.DOWNLOAD_SERVICE);
+	try:
+		manager.enqueue(request)
+		return True
+	except:
+		return False
